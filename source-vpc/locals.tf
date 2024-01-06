9c55fd70408b7ee976ad60d0ad238a6368784f58
isabellemsan <isabelle.muresan@cloudreach.com>
2024-01-06 21:55:15 +0100

    corrected ami image version

--- source-vpc/locals.tf
+++ source-vpc/locals.tf
@@ -9,7 +9,10 @@ locals {
   project_name            = "CAPCI-Isabelle"
   environment             = "Development"
   challenge               = "migration-rehost"
-
+  ubuntu_ami_name                = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+  ubuntu_ami_owner = "099720109477"
+  instance_type_db ="m5.large"
+  instance_type_ws ="t3.medium"
 
   tags = {
     ProjectName = local.project_name
